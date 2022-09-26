ff7d3ffec6011ed7a99e2b2b0b036e109658019c
Jesper Andersson <jesper.ja.andersson@folksam.se>
2022-09-26 14:49:35 +0200

    fixed output

--- Kattis/99 Problems/99_problmes.go
+++ Kattis/99 Problems/99_problmes.go
@@ -4,26 +4,31 @@ import (
 	"fmt"
 )
 
+// https://open.kattis.com/problems/99problems
 func main() {
 
 	var n int
 	var i, y, x int
 	fmt.Scan(&n)
 	x = n
+	
 	for n % 100 != 99 {
 		n += 1
 		i += 1
 	}
 	for x % 100 != 99{
-		if x == 0{
+		if x == 0 {
 			break
 		}
 		x -= 1
 		y += 1
 	}
-
-	// Check the one which is closest to the number ending with 99
-	// TODO
 	
-
+	if i == y {
+		fmt.Println(n)
+	}else if i < y{
+		fmt.Println(n)
+	}else{
+		fmt.Println(x)
+	}
 }
